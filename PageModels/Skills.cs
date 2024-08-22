4421ffe1cef132a9f44f8d80c4669d88aa12886e
WaysoonProgramms <krokodilovanton@gmail.com>
2024-08-22 10:59:25 +0300

    commit fix Восстановление файлов страницы скилов.

---
+++ PageModels/Skills.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Leo.WindowModels;
+
+namespace Leo.PageModels;
+
+public partial class Skills
+{
+    public Skills()
+    {
+        InitializeComponent();
+    }
+
+    private void back(object sender, RoutedEventArgs e)
+    {
+        MainWindow.backAboutPage();
+    }
+}
